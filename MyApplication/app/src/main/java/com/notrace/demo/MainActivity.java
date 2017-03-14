53ac2138bcc0fb9d7f0fe040ad7a95b0234c2bc1
messnoTrace <cy_nforget@126.com>
2017-03-14 16:05:30 +0800

    Fix demo

--- MyApplication/app/src/main/java/com/notrace/demo/MainActivity.java
+++ MyApplication/app/src/main/java/com/notrace/demo/MainActivity.java
@@ -2,27 +2,29 @@ package com.notrace.demo;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.notrace.systembar.SystemBarTintManager;
+import com.notrace.systembar.StatusBarCompat;
 
 public class MainActivity extends Activity {
-    protected SystemBarTintManager mTintManager;
+//    protected SystemBarTintManager mTintManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+        StatusBarCompat.translucentStatusBar(this,true);
+//
+//        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
+//            setTranslucentStatus(true);
+//        }
+//        mTintManager=new SystemBarTintManager(this);
+//        mTintManager.setStatusBarTintEnabled(true);
+//        mTintManager.setStatusBarTintResource(R.color.navigation_color);
 
 
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
-            setTranslucentStatus(true);
-        }
-        mTintManager=new SystemBarTintManager(this);
-        mTintManager.setStatusBarTintEnabled(true);
-        mTintManager.setStatusBarTintResource(R.color.navigation_color);
 
     }
 
